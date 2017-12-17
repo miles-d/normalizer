eac3899d728548264c65cc92c4b35fa73080952a
Milosz Danczak <milosz.danczak@gmail.com>
2017-12-17 16:07:43 +0100

    setup and teardown test files

--- test_normalizer.py
+++ test_normalizer.py
@@ -1,9 +1,31 @@
-import unittest
+import unittest, os
 from normalizer import Normalizer
 
 class NormalizerTest(unittest.TestCase):
   def setUp(self):
     self.normalizer = Normalizer()
+    dirs = [
+        'testdir',
+        'testdir/subdir',
+        'testdir/subdir/nasty subdir',
+        'testdir/subdir/subsubdir',
+        ]
+    for d in dirs:
+      os.mkdir(d)
+
+    files = [
+        'testdir/one',
+        'testdir/two',
+        'testdir/subdir/foo bar',
+        'testdir/subdir/three',
+        'testdir/subdir/subsubdir/four',
+        'testdir/subdir/nasty subdir/nasty file',
+        ]
+    for f in files:
+      os.mknod(f)
+
+  def tearDown(self):
+    os.system('rm -rf testdir')
 
   def test_removes_quotes(self):
     self.assertEqual('bar', self.normalizer.normalize('"b\'a\'r"'))
@@ -22,6 +44,13 @@ class NormalizerTest(unittest.TestCase):
 
   def test_only_normalizes_basename(self):
     self.assertEqual('one two/three_four', self.normalizer.normalize('one two/three four'))
+
+  def test_checks_if_needs_normalization(self):
+    self.assertTrue(self.normalizer.needs_normalizing('foo bar'))
+    self.assertFalse(self.normalizer.needs_normalizing('foo_bar'))
+
+  def test_does_not_mutilate_extension(self):
+    self.assertEqual('foo_bar.mp3', self.normalizer.normalize('foo bar.mp3'))
 
   def test_sees_files_from_directory(self):
     expected_names = {
@@ -44,10 +73,3 @@ class NormalizerTest(unittest.TestCase):
         'subdir/nasty subdir/nasty_file',
         }
     self.assertEqual(normalized_names, set(self.normalizer.normalize_in_dir('testdir')))
-
-  def test_checks_if_needs_normalization(self):
-    self.assertTrue(self.normalizer.needs_normalizing('foo bar'))
-    self.assertFalse(self.normalizer.needs_normalizing('foo_bar'))
-
-  def test_does_not_mutilate_extension(self):
-    self.assertEqual('foo_bar.mp3', self.normalizer.normalize('foo bar.mp3'))
